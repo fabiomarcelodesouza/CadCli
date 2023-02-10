5625625af61f5453e89283f33e40faf47ccd7f1a
Fabio Marcelo de Souza <115677711+fabiomarcelodesouza@users.noreply.github.com>
2023-02-10 20:31:52 -0300

    Update ClientesController.cs

--- src/CadCli.Api/Controllers/ClientesController.cs
+++ src/CadCli.Api/Controllers/ClientesController.cs
@@ -11,7 +11,7 @@ public class ClientesController : ControllerBase
     private static List<Cliente> dados = new List<Cliente> {
 
         new Cliente{Id=1, Nome= "Fabiano Nalin", Idade = 43, Sexo = "Masculino"},
-        new Cliente{Id=2, Nome= "Priscila Mitui", Idade = 44, Sexo = "Feminino"},
+        //new Cliente{Id=2, Nome= "Priscila Mitui", Idade = 44, Sexo = "Feminino"},
         new Cliente{Id=3, Nome= "Raphael Akyu", Idade = 23, Sexo = "Masculino"},
         new Cliente {Id=4, Nome= "Nair Goes", Idade = 80, Sexo = "Feminino"},
     };
